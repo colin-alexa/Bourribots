ceea483c6b95bcb274203274056c9c978dfabeb8
colin <colinarobinson@gmail.com>
2014-01-31 16:13:47 -0800

    start of jumblr-clojure work

---
+++ sandbox/project.clj
@@ -0,0 +1,5 @@
+(defproject project "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [com.tumblr/jumblr "0.0.6"]]
+  :main project.core)
---
+++ sandbox/src/project/core.clj
@@ -0,0 +1,14 @@
+(ns project.core
+    (:import (com.tumblr.jumblr JumblrClient)))
+
+(def client (new JumblrClient 
+		 "WDXZgPZj1bkFbQIrEYNKMTy4UEjKYDxoVvQTKFUZj0LxHiQuJc"
+		 "J1GgEilzQL794Tt5BS0gxv2wMkhiqm7rDEknbAmi1NjNC0IegJ"))
+
+(def exblog (. client blogInfo "gamzee.tumblr.com"))
+
+;(defn get-name [] 
+;      (print 
+;	(. charmides getName)))
+
+(defn -main [] "Fuck you, no")
