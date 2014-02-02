49820e1a1e1bc7301c274bca362603c9e5528e2e
colin <colinarobinson@gmail.com>
2014-02-02 01:47:30 -0800

    changed to jumblr:0.0.7-SNAPSHOT

--- sandbox/project.clj
+++ sandbox/project.clj
@@ -1,5 +1,5 @@
 (defproject project "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [com.tumblr/jumblr "0.0.6"]]
+		 [com.tumblr/jumblr "0.0.7-SNAPSHOT"]]
   :main project.core)
