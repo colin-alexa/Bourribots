81223e4cc9a6e66ca8b1e82a061964b55d2d7107
colin <colinarobinson@gmail.com>
2014-03-13 02:09:04 -0700

    built database (postgresql 9.1)

--- sandbox/project.clj
+++ sandbox/project.clj
@@ -1,5 +1,7 @@
 (defproject project "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [com.tumblr/jumblr "0.0.7-SNAPSHOT"]]
+		 [com.tumblr/jumblr "0.0.7-SNAPSHOT"]
+		 [postgresql/postgresql "8.4-702.jdbc4"]
+		 [korma "0.3.0-RC5"]]
   :main project.core)
