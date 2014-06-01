f6da4dc6a038ea8c8857c2a384c6cdb1420d3585
colin <colinarobinson@gmail.com>
2014-06-01 14:10:17 -0700

    trying again

---
+++ sandbox/.lobos_stash.clj
@@ -0,0 +1,12 @@
+
+(create
+ (table
+  :users
+  (integer :id :auto-inc :primary-key)
+  (varchar :name 127)))
+
+(create
+ (table
+  :users
+  (integer :id :auto-inc :primary-key)
+  (varchar :name 127)))
