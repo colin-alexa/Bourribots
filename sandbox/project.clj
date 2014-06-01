c36d41986e785679e3ac7769cbae6ddeb7dce8cc
colin <colinarobinson@gmail.com>
2014-06-01 14:02:01 -0700

    reorganizing and updating project.db

--- sandbox/project.clj
+++ sandbox/project.clj
@@ -1,8 +1,11 @@
 (defproject project "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+		 [org.clojure/java.jdbc "0.1.1"]
+		 [org.clojure/tools.macro "0.1.1"]
 		 [com.tumblr/jumblr "0.0.7-SNAPSHOT"]
 		 [postgresql/postgresql "8.4-702.jdbc4"]
-		 [korma "0.3.0-RC5"]
+		 [korma "0.3.0-beta9"]
+		 [c3p0/c3p0 "0.9.1.2"]
 		 [lobos "1.0.0-beta1"]]
   :main project.core)
